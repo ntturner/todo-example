63ba84fd845971bf902d47b4ac317a635b6a8471
Nate Turner <nathanturner595@gmail.com>
2020-02-12 20:39:44 -0700

    Fixed variable call.

--- server/_config/config.js
+++ server/_config/config.js
@@ -18,7 +18,7 @@ export const configOptions = {
     userCookieName: 'user'
   },
   testProduction: {
-    db: `mongodb://cloud:I will never be a memory@cluster0-shard-00-00-0zx0k.mongodb.net:27017,cluster0-shard-00-01-0zx0k.mongodb.net:27017,cluster0-shard-00-02-0zx0k.mongodb.net:27017/${testdbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    db: `mongodb://cloud:I will never be a memory@cluster0-shard-00-00-0zx0k.mongodb.net:27017,cluster0-shard-00-01-0zx0k.mongodb.net:27017,cluster0-shard-00-02-0zx0k.mongodb.net:27017/${testDbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
     port: process.env.PORT || 3001,
     userCookieName: 'user'
   }
