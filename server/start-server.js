7fa080e30599623fe4eb5478bf38cd568e1dc40a
Nate Turner <nathanturner595@gmail.com>
2020-02-13 12:15:15 -0700

    Test change.

--- server/start-server.js
+++ server/start-server.js
@@ -16,7 +16,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'DELETE, POST, GET, OPTIONS')
   next()
 })
-
+// A change
 configureExpress(app, config)
 
 configureMongoose(config, env)
